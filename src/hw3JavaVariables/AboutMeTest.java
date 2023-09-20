af75b9a6dbcde59b9b932e85ba10b707d667f8c8
Ruhul-Kuddus <mdrhlkdus@gmail.com>
2023-09-20 00:57:36 -0400

    home work

--- src/hw3JavaVariables/AboutMeTest.java
+++ src/hw3JavaVariables/AboutMeTest.java
@@ -5,14 +5,12 @@ public class AboutMeTest {
 	public static void main(String[] args) {
 		AboutMe aboutMe = new AboutMe(); // Constructor initialized
 
-		System.out.println("-------------------------------------------------------------------------------");
-
 		System.out.println("My Name: " + aboutMe.myName + "\nMy Age: " + aboutMe.myAge + "\nMy Apartment Rent: "
 				+ aboutMe.myApartmentRent + "\nYearly Salary: " + aboutMe.myYearlySalary + "\nMy Bank Balance: "
 				+ aboutMe.myBankBalance + "\nGender: " + aboutMe.myGender + "\nMy Height: " + aboutMe.myHeight
 				+ "\nMy Grade: " + aboutMe.myGrade + "\nAm I US Citizen? Ans: " + aboutMe.usCitizen);
 
-		// method initialized here.
+		// method initialized
 		aboutMe.aboutMe();
 
 	}
